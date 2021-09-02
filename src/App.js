e34fe5566b6be44467fc86e3c09b75f4b4b68532
slimbis <limberopoulos@gmail.com>
2021-09-02 11:44:21 +0300

    version - added mits and auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Version 2</h1>
+        <h1>Mitsos Kalimera :-)</h1>
+        <h2>We now have Auth!</h2>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
